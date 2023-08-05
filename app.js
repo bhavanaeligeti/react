17ba798cfb8f55dc07e08974038d471903735c4c
Bhavana <bhavanaeligeti5@gmail.com>
2023-08-05 18:00:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Hello everyone....";
+
+// const root = document.getElementById("root");
+
+// root.appendChild(heading);
+
+// REACT
+
+// const heading = React.createElement("h1", {
+//     id : "title",
+//     className : "header",
+//     style : {
+//         color: "red",
+//         background: "green"
+//     }
+// }, "Hello Everyone...");
+// console.log(heading)
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+
+
+// const heading1 = React.createElement("h1", {
+//     id : "title",
+//     style : {
+//         color: "red"
+//     }
+// }, "RED");
+
+// const heading2 = React.createElement("h2", {
+//     id : "title",
+//     style : {
+//         color: "blue"
+//     }
+// }, "BLUE");
+
+// const container = React.createElement("div", {
+//     id: "container",
+//     style: {
+//         background: "cyan"
+//     }
+// }, 
+// [heading1, heading2]
+// )
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(container);
+
